af20c587c181087ee2b4112921c12f5d71295e94
bitaaliya <bitalyax@gmail.com>
2024-06-06 13:55:25 +0800

    first commit

---
+++ app/src/main/java/com/dicoding/capstone/ui/fragment/changeprofile/ChangeProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.dicoding.capstone.ui.fragment.changeprofile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.capstone.databinding.FragmentChangeProfileBinding
+
+class ChangeProfileFragment : Fragment() {
+
+    private var _binding: FragmentChangeProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(ChangeProfileViewModel::class.java)
+
+        _binding = FragmentChangeProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
